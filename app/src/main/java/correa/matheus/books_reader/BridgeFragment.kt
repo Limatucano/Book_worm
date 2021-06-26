5cd08174810e476b37becd1bc92d96992fcaa32d
Matheus Lima <matheusmt31@gmail.com>
2021-06-26 18:29:37 -0300

    @feat: bar code implemented

--- app/src/main/java/correa/matheus/books_reader/BridgeFragment.kt
+++ app/src/main/java/correa/matheus/books_reader/BridgeFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BridgeFragment : Fragment() {
 
@@ -22,6 +25,13 @@ class BridgeFragment : Fragment() {
         return inflater.inflate(R.layout.bridge_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val btn_scan = view.findViewById<Button>(R.id.btn_scan)
+
+        btn_scan.setOnClickListener {
+            view.findNavController().navigate(R.id.action_bridgeFragment_to_readBarCodeFragment)
+        }
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(BridgeViewModel::class.java)
