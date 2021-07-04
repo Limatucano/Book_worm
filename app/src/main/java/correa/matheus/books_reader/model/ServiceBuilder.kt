521036a577cd1ef7cd2919cb4ea0dd60150bb44c
Matheus Lima <matheusmt31@gmail.com>
2021-07-03 21:22:02 -0300

    created api builder using retrofit

--- app/src/main/java/correa/matheus/books_reader/model/ServiceBuilder.kt
+++ app/src/main/java/correa/matheus/books_reader/model/ServiceBuilder.kt
@@ -1,4 +1,19 @@
 package correa.matheus.books_reader.model
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ServiceBuilder {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.googleapis.com/books/v1/volumes/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
 }
